5ab0bd22329cb14e2c5bc3f47dd8537cf9fc3727
Aravind G Pillai <aravindgpillai666@gmail.com>
2025-07-17 15:40:51 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const bodyParser = require('body-parser');
+const User = require('./models/User');
+const Camp = require('./models/Camp');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+mongoose.connect('your-mongodb-url', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  await new User({ username, password }).save();
+  res.send('User Registered!');
+});
+
+app.get('/camps', async (req, res) => {
+  const camps = await Camp.find();
+  res.render('camps', { camps });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Running on http://localhost:${PORT}`));
