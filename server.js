bb87bd7d24a8d3faeaf57d83769a0eb3e93515cc
Anugraha Varghese <anuvargheseav123@gmail.com>
2025-07-18 17:53:00 +0530

    hu

--- server.js
+++ server.js
@@ -23,17 +23,7 @@ const DB_PATH = path.join(__dirname, "database")
 const USERS_FILE = path.join(DB_PATH, "users.json")
 const CAMPS_FILE = path.join(DB_PATH, "camps.json")
 const SELECTIONS_FILE = path.join(DB_PATH, "selections.json")
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log('Connected to MongoDB');
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-})
-.catch((err) => {
-  console.error('Error connecting to MongoDB:', err);
-});
+
 
 
 // Initialize database
